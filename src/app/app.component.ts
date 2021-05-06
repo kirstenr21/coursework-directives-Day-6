609c2d80b77876337d3bbf886b26b7020bf94fc7
kirstenr21 <82554708+kirstenr21@users.noreply.github.com>
2021-05-05 20:25:49 -0500

    Course Work Day 6
    Course Work - Directives Day 6 050521

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'coursework-directives';
+  debugger
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
+  value = 10;
 }
